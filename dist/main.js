1adcda10e247469d997bcf323512ed43a363c063
Maxime Faure <maxime.faure93@gmail.com>
2025-04-20 19:35:13 +0200

    [ADD] - dist module

---
+++ dist/main.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const swagger_1 = require("@nestjs/swagger");
+const app_module_1 = require("./app.module");
+const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+            enableImplicitConversion: true,
+        },
+    }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('NestJs Masterclass - Blog app API')
+        .setDescription('Use the base API URL as http://localhost:3000')
+        .setTermsOfService('http://localhost:3000/terms-of-service')
+        .setLicense('MIT License', 'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt')
+        .addServer('http://localhost:3000')
+        .setVersion('1.0')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    await app.listen(3000);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
