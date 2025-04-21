f2ad66f452d8d2b77249af89f3919aca618631e2
Maxime Faure <maxime.faure93@gmail.com>
2025-04-21 16:51:52 +0200

    [ADD] - tags, connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,36 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
+/**
+ * Importing Entities
+ * */
+import { User } from './users/user.entity';
+import { TagsModule } from './tags/tags.module';
+
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [User],
+        synchronize: true,
+        port: 5433,
+        username: 'postgres',
+        password: 'test',
+        host: 'localhost',
+        database: 'nestjs-',
+      }),
+    }),
+    TagsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
