76897cbf1f7629b140fd49b01cb781a2272bf9e2
Maxime Faure <maxime.faure93@gmail.com>
2025-05-13 09:51:20 +0200

    [ADD] - mail service and template

--- src/mail/providers/mail.service.ts.service.ts
+++ src/mail/providers/mail.service.ts.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class MailServiceTsService {}
+export class MailServiceTsService {
+  constructor(
+    /**
+     * Inject mailerService
+     */
+    private mailerService: MailerService,
+  ) {}
+
+  public async sendUserWelcome(user: User): Promise<void> {
+    await this.mailerService.sendMail({
+      to: user.email,
+      from: `Onboarding Team <support@nestjs-blog.com>`,
+      subject: 'Welcome to NestJs Blog',
+      template: './welcome',
+      context: {
+        name: user.firstName,
+        email: user.email,
+        loginUrl: 'http://localhost:3000',
+      },
+    });
+  }
+}
