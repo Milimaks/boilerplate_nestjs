e4b31bd25d081fb75067a08f2a2f2c00f8791245
Maxime Faure <maxime.faure93@gmail.com>
2025-05-13 09:44:16 +0200

    [ADD] - config of mail.module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,39 @@
-import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { Global, Module } from '@nestjs/common';
 import { MailServiceTsService } from './providers/mail.service.ts.service';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
+@Global()
 @Module({
-  providers: [MailServiceTsService]
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('appConfig.mailHost'),
+          secure: false,
+          port: 2525,
+          auth: {
+            user: config.get('appConfig.smtpUsername'),
+            pass: config.get('appConfig.smtpPassword'),
+          },
+          default: {
+            from: `My Blog <no-preply@nestjs-blog.com>`,
+          },
+          template: {
+            dir: join(__dirname) + 'templates',
+            adapter: new EjsAdapter(),
+            options: {
+              strict: false,
+            },
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [MailServiceTsService],
+  exports: [MailServiceTsService],
 })
 export class MailModule {}
